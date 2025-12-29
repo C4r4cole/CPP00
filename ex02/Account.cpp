0c1dde28fc7831a802930f0c8f52c9c879a98682
C4r4cole <c4r4cole.chevalerie@gmail.com>
2025-12-29 20:00:48 +0100

    ex02 ongoing

---
+++ ex02/Account.cpp
@@ -0,0 +1,42 @@
+// ************************************************************************** //
+//                                                                            //
+//                Account.cpp for GlobalBanksters United                      //
+//                Created on  : Thu Nov 20 19:43:15 1989                      //
+//                Last update : Wed Jan 04 14:54:06 1992                      //
+//                Made by : Brad "Buddy" McLane <bm@gbu.com>                  //
+//                                                                            //
+// ************************************************************************** //
+
+#include "Account.hpp"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+Account::Account(int initial_deposit)
+{
+}
+Account::~Account(void)
+{
+}
+
+int	Account::getNbAccounts(void)
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount(void)
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits(void)
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return (_totalNbWithdrawals);
+}
+
+static void	displayAccountsInfos( void );
